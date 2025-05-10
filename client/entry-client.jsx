8b17948da1bb677d7a6b1d214f78f814bb6c3a6a
Tommy Tran <trantommy.2001@gmail.com>
2025-05-10 16:53:09 +0200

    fix MIME checking error

--- client/entry-client.jsx
+++ client/entry-client.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
-import "./base.css";
 
 ReactDOM.hydrateRoot(
   document.getElementById("root"),
